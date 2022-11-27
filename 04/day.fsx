8ffcbe1b27ecf1b48f538cc2a3acd35c2a4b7dbe
Jon Ekdahl <jon.ekdahl@valtech.com>
2022-11-27 16:41:31 +0000

    Day 04. part 2

--- 04/day.fsx
+++ 04/day.fsx
@@ -56,3 +56,28 @@ let sumUnmarked (isDrawn: int -> bool) (board: Board): int =
 let winningBoard, numbersDrawn = findWinningBoard
 let score = (sumUnmarked (isDrawn numbersDrawn) winningBoard) * (Array.last numbersDrawn)
 printfn $"Part 1: {score}"
+
+
+let findLastWinningBoard (allBoards: Board list) =
+    let mutable drawIndex = 0
+    let mutable numbersDrawn = [||]
+
+    let rec _find (boards: Board list) =
+        let isDrawnX = isDrawn numbersDrawn
+
+        if boards.Length = 1 && (boardHasBingo isDrawnX boards.Head) then
+            boards.Head
+        else
+            drawIndex <- drawIndex + 1
+            numbersDrawn <- numbers[0..drawIndex]
+
+            boards
+            |> List.filter (fun board -> not (boardHasBingo isDrawnX board))
+            |> _find
+    
+    let lastWinningBoard = _find allBoards
+    lastWinningBoard, numbersDrawn
+    
+let lastWinningBoard, numbersDrawnLast = findLastWinningBoard boards
+let scoreLast = (sumUnmarked (isDrawn numbersDrawnLast) lastWinningBoard) * (Array.last numbersDrawnLast)
+printfn $"Part 2: {scoreLast}"
